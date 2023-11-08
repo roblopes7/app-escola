873ece88947cd52268de5bf4fcdac757c3cb3cbc
roblopes7 <42593177+roblopes7@users.noreply.github.com>
2023-11-08 07:09:34 -0300

    Config axios

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import axios from '../../services/axios';
 import { Container } from '../../styles/GlobalStyles';
 import { Paragrafo, Title } from './styled';
 
 export default function Login() {
+  React.useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/alunos');
+      console.log(response);
+    }
+
+    getData();
+  }, []);
+
   return (
     <Container>
       <Title>
